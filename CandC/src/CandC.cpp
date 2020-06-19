288dcb54bee0cd291d894bff51c727e8c8e31fe0
Leo <leonardheininger1@gmail.com>
2020-06-19 18:12:04 +0200

    + init

---
+++ CandC/src/CandC.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int main()
+{
+	std::cout << "Init CanC" << std::endl;
+	std::cin.get();
+	return 0;
+}
---
+++ Zombie/src/Zombie.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int main()
+{
+	std::cout << "Init Zombie" << std::endl;
+	std::cin.get();
+	return 0;
+}
